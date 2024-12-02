9d7b6bb32575034c447dedf4c1a473420843b3b5
matthewbernhardt23 <matt.bernhardt@yahoo.com>
2024-12-02 09:45:21 -0700

    deleted scripts

--- scripts/stop_docker.sh
+++
@@ -1,9 +0,0 @@
-  #!/bin/bash
-CONTAINER_ID=$(docker ps -q --filter "name=thoughtmap-backend")
-if [ ! -z "$CONTAINER_ID" ]; then
-  echo "Stopping existing container..."
-  docker stop $CONTAINER_ID
-  docker rm $CONTAINER_ID
-else
-  echo "No existing container found. Skipping stop."
-fi
