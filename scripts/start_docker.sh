3c18300176d4701e52eed3babcde916ebc4e123c
matthewbernhardt23 <matt.bernhardt@yahoo.com>
2024-12-05 12:36:48 -0700

    updated script

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -3,11 +3,11 @@
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 761018876037.dkr.ecr.us-east-2.amazonaws.com
 
 # Pull the latest image
-docker pull 761018876037.dkr.ecr.us-east-2.amazonaws.com/thoughtmap-backend:latest
+docker pull 761018876037.dkr.ecr.us-east-2.amazonaws.com/thoughtmap/service/data
 
 # Run the new container
 docker run -d \
   --name thoughtmap-backend \
   -p 8080:8080 \
-  761018876037.dkr.ecr.us-east-2.amazonaws.com/thoughtmap-backend:latest
+  761018876037.dkr.ecr.us-east-2.amazonaws.com/thoughtmap/service/data
 
